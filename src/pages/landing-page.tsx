59c901f3d3bb0a211c2ce146add2d057f454f896
ajmal-m <ajua76774@gmail.com>
2025-09-04 17:41:38 +0530

    Font update

--- src/pages/landing-page.tsx
+++ src/pages/landing-page.tsx
@@ -11,20 +11,28 @@ const LandingPage = memo(() => {
                 "
             >
                 <img src={Logo} alt="logo-landing-page"/>
-                <h1 className="text-[32px] font-bold text-[#2563EB] leading-[auto] tracking-[15%]">FinSight</h1>
-                <p className="text-[#111827] text-[36px] leading-[auto] font-semibold text-center">
+                <h1 className="text-[32px] font-bold text-[#2563EB] leading-[auto] tracking-[15%] font-lora">FinSight</h1>
+                <p className="text-[#111827] text-[36px] leading-[auto] font-semibold text-center font-inter">
                     Start Tracking Smarter.
                     <br/>
                     Take control of your money with AI-powered insights.
                 </p>
-                <p className="text-[#6B7280] text-[24px] italic">
+                <p className="text-[#6B7280] text-[24px] italic font-inter text-center">
                     Track expenses, set budgets, and get real-time financial advice — all in one place.
                 </p>
                 <div className="flex items-center gap-[32px] mt-[32px]">
-                    <button className="cursor-pointer bg-[#2563EB] text-[#FFFFFF] rounded-[50px] w-[124px] h-[45px] font-semibold text-[24px]">
+                    <button className="
+                        cursor-pointer bg-[#2563EB] text-[#FFFFFF] 
+                        rounded-[50px] w-[124px] h-[45px] font-semibold 
+                        text-[24px] font-inter
+                    ">
                         SIGNUP
                     </button>
-                    <button className="cursor-pointer border border-[#2563EB] bg-[#FFFFFF] text-[#2563EB] rounded-[50px] w-[106px] h-[45px] font-semibold text-[24px]">
+                    <button className="
+                        cursor-pointer border border-[#2563EB] bg-[#FFFFFF] text-[#2563EB] 
+                        rounded-[50px] w-[106px] h-[45px] font-semibold 
+                        text-[24px] font-inter
+                    ">
                         LOGIN
                     </button>
                 </div>
