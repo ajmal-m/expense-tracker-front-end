e0aed2a4acf51e0c6556bc524e4250049c108e47
ajmal-m <ajua76774@gmail.com>
2025-09-05 11:59:19 +0530

    Login page update

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { lazy, Suspense } from 'react';
 
 const LandingPage = lazy(() => import('./pages/landing-page'));
 const SignUpPage = lazy(() => import('./pages/sign-up-page'));
+const LoginPage = lazy(() => import('./pages/log-in-page'));
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Suspense fallback={<>Loading...</>}><LandingPage/></Suspense>}/>
-        <Route path='/login' element={<Suspense fallback={<>Loading...</>}><>Login Page</></Suspense>}/>
+        <Route path='/login' element={<Suspense fallback={<>Loading...</>}><LoginPage/></Suspense>}/>
         <Route path='/sign-up' element={<Suspense fallback={<>Loading...</>}><SignUpPage/></Suspense>}/>
       </Routes>
     </>
