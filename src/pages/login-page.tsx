eec56d7a2cf7dbda90ae8b76f703b58a34a120e5
ajmal-m <ajua76774@gmail.com>
2025-09-05 10:18:18 +0530

    Login page completed

--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -1,6 +1,7 @@
 import { memo } from "react";
 import AuthLayout from "../layouts/auth-layout";
 import LogoImage from '../assets/LogoSmall.svg';
+import GoogleIcon from '../assets/google_icon.svg';
 
 const LoginPage = memo(() => {
     return(
@@ -11,7 +12,7 @@ const LoginPage = memo(() => {
                 <h2 className="text-[24px] font-inter text-[#000000] font-[400] mt-[24px]">Join FinSight Today</h2>
                 <form>
                     <div className="flex flex-col gap-[6px] mt-[24px]">
-                        <label htmlFor="">Name</label>
+                        <label htmlFor="" className="text-[12px] text-[#111827] font-inter font-[400]">Name</label>
                         <input 
                             type="text" 
                             className="w-[433px] h-[48px] border border-[#6B7280] rounded-[8px] 
@@ -19,7 +20,7 @@ const LoginPage = memo(() => {
                         />
                     </div>
                     <div className="flex flex-col gap-[6px] mt-[24px]">
-                        <label htmlFor="">Email Address</label>
+                        <label htmlFor="" className="text-[12px] text-[#111827] font-inter font-[400]">Email Address</label>
                         <input 
                             type="text" 
                             className="w-[433px] h-[48px] border border-[#6B7280] rounded-[8px] 
@@ -27,7 +28,7 @@ const LoginPage = memo(() => {
                         />
                     </div>
                     <div className="flex flex-col gap-[6px] mt-[24px]">
-                        <label htmlFor="">Password</label>
+                        <label htmlFor="" className="text-[12px] text-[#111827] font-inter font-[400]">Password</label>
                         <input 
                             type="text" 
                             className="w-[433px] h-[48px] border border-[#6B7280] rounded-[8px] 
@@ -35,7 +36,7 @@ const LoginPage = memo(() => {
                         />
                     </div>
                     <div className="flex flex-col gap-[6px] mt-[24px]">
-                        <label htmlFor="">Confirm Password</label>
+                        <label htmlFor="" className="text-[12px] text-[#111827] font-inter font-[400]">Confirm Password</label>
                         <input 
                             type="text" 
                             className="w-[433px] h-[48px] border border-[#6B7280] rounded-[8px] 
@@ -52,6 +53,42 @@ const LoginPage = memo(() => {
                         >
                             CREATE ACCOUNT
                         </button>
+                    </div>
+                    <div className="flex items-center justify-center mt-[12px]">
+                        <span className="text-[12px] font-[400] font-inter text-[#111827]">
+                            OR
+                        </span>
+                    </div>
+                    <div className="mt-[12px]">
+                        <button
+                            className="
+                                w-[433px] h-[38px] flex justify-center
+                                items-center gap-[6px]
+                                border-[2px] border-[#2563EB] rounded-[8px]
+                                cursor-pointer
+                            "
+                        >
+                            <img src={GoogleIcon} alt="google-icon" />
+                            <p
+                                className="
+                                    text-[16px] font-bold font-inter
+                                    text-[#2563EB] tracking-[0%]
+                                "
+                            >Continue with Google</p>
+                        </button>
+                    </div>
+                    <div className="mt-[12px]">
+                        <p
+                            className="
+                                text-[12px] text-[#111827] italic font-inter text-center
+
+                            "
+                        >
+                            Already have an account? <span className="text-[#2563EB]">Login</span>
+                        </p>
+                        <p className="mt-[12px] text-[#111827] text-[12px] italic font-inter text-center">
+                            🔒 Your data is encrypted and never shared without your consent.
+                        </p>
                     </div>
                 </form>
             </div>
