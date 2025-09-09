1c751422030b6637e34fbfa7e3b3c9840b122fdd
ajmal-m <ajua76774@gmail.com>
2025-09-09 16:18:01 +0530

    Code refactoring

--- src/components/models/add-category-budget.tsx
+++ src/components/models/add-category-budget.tsx
@@ -1,26 +1,43 @@
-import { memo } from "react";
+import { memo, useCallback, useState } from "react";
 import { useSelector } from "react-redux";
 import { type RootState } from "../../store/store";
 import Button from "../reusable/button";
+import ImageInput from "../reusable/input-image";
 
 const AddCategoryBudgetModal = memo(({ close }: { close : () => void }) => {
-      const categories = useSelector((store: RootState) => store.category.categoryies);
+    const categories = useSelector((store: RootState) => store.category.categoryies);
+    const [categoryBudget, setCategoryBudget] = useState({
+        categoryId: '',
+        amount:''
+    });
+
+    const updateCategoryBudget = useCallback((e: { target: { name: any; value: any; }; } ) => {
+        setCategoryBudget((c) => ({ ...c, [e.target.name] : [e.target.value]}));
+    }, [categoryBudget])
     return(
         <div 
             className="    
                 bg-[#FFFFFF] rounded-[24px] 
                 flex flex-col items-center
-                p-[32px]
+                p-[32px] gap-2
             "
         >
-            <select name="category" id="category" >
+            <select name="category" id="category" className="w-[584px] h-[60px] border border-[#6B7280] rounded-[8px] pl-[40px]
+                    text-[20px] font-inter font-[400] text-[#6B7280]"  onChange={updateCategoryBudget}>
                 {
                     categories.length > 0 && categories.map((cate) => (
                         <option value={cate._id}>{cate.name}</option>
                     ))
                 }
             </select>
-            <input type="text" name="amount" id="amount" />
+
+            <ImageInput
+                value={categoryBudget.amount}
+                id="amount"
+                handleChange={updateCategoryBudget}
+                type="number"
+            />
+             
             <div className="flex justify-between items-center gap-2">
                 <Button
                     label="Cancel"
--- src/components/reusable/input-image.tsx
+++ src/components/reusable/input-image.tsx
@@ -1,30 +1,37 @@
 import { memo } from "react";
 
 type ImageInputProps = {
-    image: string;
+    image?: string;
     value: string | number ;
     handleChange ?: (e: any) => void;
     id: string;
     disabled?:boolean,
     imageTop?: number;
     imageLeft?:number;
+    type?:string;
 }
 
 export const ImageInput = memo((
-    { image  , value, handleChange , id , disabled , imageTop, imageLeft}: ImageInputProps
+    { image  , value, handleChange , id , disabled , imageTop, imageLeft, type}: ImageInputProps
 ) => {
     return(
         <div className="relative">
-            <img 
-                src={image} 
-                alt={id} 
-                className="absolute"
-                style={{
-                    top: imageTop+'px',
-                    left: imageLeft+'px',
-                }} 
-            />
-            <input type="text" name="name" id="name"
+            {
+                image && (
+                    <img 
+                        src={image} 
+                        alt={id} 
+                        className="absolute"
+                        style={{
+                            top: imageTop+'px',
+                            left: imageLeft+'px',
+                        }} 
+                    />
+                )
+            }
+            <input 
+                type={type ? type : "text"}
+                name={id} id={id}
                 className="w-[584px] h-[60px] border border-[#6B7280] rounded-[8px] pl-[40px]
                     text-[20px] font-inter font-[400] text-[#6B7280]
                 "
