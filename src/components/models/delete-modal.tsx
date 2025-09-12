0dca37e828c4c9407be125ae94526d11f365385a
ajmal-m <ajua76774@gmail.com>
2025-09-12 22:55:22 +0530

    Delete modal ui updates

--- src/components/models/delete-modal.tsx
+++ src/components/models/delete-modal.tsx
@@ -1,12 +1,15 @@
 import { memo } from "react";
+import Button from "../reusable/button";
 
 
 const DeleteModal = memo(({ close, confirm }: { close : () => void , confirm: () => void}) => {
     return(
-        <div className="p-2 bg-blue-900 text-white rounded">
-            <p>Do u want to delete</p>
-            <button onClick={close}>cancel</button>
-            <button onClick={confirm}> Confirm</button>
+        <div className="p-6 bg-blue-900 text-white rounded">
+            <p>Do u want to delete ?</p>
+            <div className="flex items-center gap-2 mt-[20px]">
+                <Button label="Cancel" onClick={close} color="white" bgColor="blue"/>
+                <Button label="Confirm" onClick={confirm} color="white" bgColor="red"/>
+            </div>
         </div>
     )
 });
